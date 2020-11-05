d908a77aa02eae38cb24b4534e7a979594f7fa0e
Daniel Vega <danielalexvega@gmail.com>
2020-11-05 10:08:48 -0600

    work on navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import Navigation from './navigation';
 function App() {
   return (
     <div>
-
+      <Navigation />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+import './assets/base.scss';
+
 ReactDOM.render(
   <App />,
   document.querySelector('#root')
