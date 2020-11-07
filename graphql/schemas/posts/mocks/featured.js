2c500590cbf4675223c4b775a34b40bdfc19f0a8
Daniel Vega <danielalexvega@gmail.com>
2020-11-07 16:28:08 -0600

    work on backend

---
+++ graphql/schemas/posts/mocks/featured.js
@@ -0,0 +1,33 @@
+
+const moment = require('moment');
+
+module.exports = [
+  {
+    title: 'Can anyone code?',
+    date: moment().format('MMMM DD, YYYY'),
+    categories: ['Tech Culture', 'Tech News'],
+    link: '#',
+    image: 'anyone_can_code.jpg',
+  },
+  {
+    title: 'Using AWS S3 for Storing Blog Images',
+    date: moment().format('MMMM DD, YYYY'),
+    categories: ['Cloud'],
+    link: '#',
+    image: 'anyone_can_code.jpg',
+  },
+  {
+    title: 'Popular Programming Languages in 2020',
+    date: moment().format('MMMM DD, YYYY'),
+    categories: ['Tech News', 'Tech Culture'],
+    link: '#',
+    image: 'anyone_can_code.jpg'
+  },
+  {
+    title: 'Brain Hacks for Learning to Program',
+    date: moment().format('MMMM DD, YYYY'),
+    categories: ['Brain Health'],
+    link: '#',
+    image: 'anyone_can_code.jpg'
+  },
+]
---
+++ graphql/schemas/posts/mocks/trending.js
@@ -0,0 +1,39 @@
+const moment = require('moment');
+
+module.exports = [
+  {
+    title: 'Software Engineer Salary in 2020',
+    date: moment().format('MMMM DD, YYYY'),
+    categories: ['Tech Culture'],
+    link: '#',
+    image: 'austin.jpg'
+  },
+  {
+    title: 'GraphQL vs REST',
+    date: moment().format('MMMM DD, YYYY'),
+    categories: ['React', 'JavaScript'],
+    link: '#',
+    image: 'austin.jpg',
+  },
+  {
+    title: 'A Day in the Life of a Programmer',
+    date: moment().format('MMMM DD, YYYY'),
+    categories: ['Tech Culture'],
+    link: '#',
+    image: 'austin.jpg'
+  },
+  {
+    title: 'Brain Hacks for Learning to Program',
+    date: moment().format('MMMM DD, YYYY'),
+    categories: ['Brain Health'],
+    link: '#',
+    image: 'austin.jpg'
+  },
+  {
+    title: 'React Vs Vue',
+    date: moment().format('MMMM DD, YYYY'),
+    categories: ['React', 'Vue'],
+    link: '#',
+    image: 'austin.jpg'
+  },
+]
